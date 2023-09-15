5d0d06a20132aea2e5d06e8395d4afc2527c2411
Maks <55331263+Guhabrus@users.noreply.github.com>
2023-09-15 14:02:44 +0300

    готовая либа для работы с flesh памятью

--- fl_lib.c
+++ fl_lib.c
@@ -5,15 +5,27 @@
  *      Author: Максим
  */
 #include "stm32f103xb.h"
-
+#include "stm32f1xx.h"
 #include "fl_lib.h"
 #include <string.h>
+
+
+//TODO добавить программирование опциональных байтов (Option bytes)
+//TODO добавить флеш мемкопи
+
 
 #define STM_FLESH_KEY1 FLASH_KEY1
 #define STM_FLESH_KEY2 FLASH_KEY2
 
-flrslt_t flesh_unlocking()
+flrslt_t flash_memcpy(uint16_t* dest, uint16_t* src, size_t size);
+
+flrslt_t flash_unlocking()
 {
+	if(!check_lock())
+	{
+		return SUCCES;
+	}
+
 	FLASH->KEYR = STM_FLESH_KEY1;
 	FLASH->KEYR = STM_FLESH_KEY2;
 
@@ -24,77 +36,58 @@ flrslt_t flesh_unlocking()
 	return SUCCES;
 }
 
+void flash_locking()
+{
+	FLASH->CR |= FLASH_CR_LOCK;
+}
+
 bool flash_available()
 {
-	while(FLASH->SR & FLASH_SR_BSY){};
+	while(FLASH->SR & FLASH_SR_BSY);
 	//TODO вернуть TIME_ERROR;
 	return true;
 }
 
-flrslt_t flesh_write(uint32_t addr, uint16_t* data, size_t size)
+flrslt_t flash_write(uint32_t addr, uint16_t* data, size_t size)
 {
-	if(MAX_FLASH_ADDR < (FLASH_OFFSET + addr) || MIN_FLASH_ADDR > (FLASH_OFFSET + addr) || addr & 1){
+	if(MAX_FLASH_ADDR < addr || MIN_FLASH_ADDR > addr || MAX_FLASH_ADDR < addr + size || addr & 1){
 		return BAD_INPUT;
 	}
-	
-	if(!flash_available()){
-		return FLESH_BLOCK;
-	}
-	
+
 	if(check_lock()){
 		return FLESH_BLOCK;
 	}
 
-	
-	FLASH->CR |= FLASH_CR_PG;
-
 	uint8_t tmp_buff[FLASH_PG_SIZE] = {0};
 
-	uint8_t num_page = (addr / FLASH_PG_SIZE);
+	uint8_t num_page = (addr - MIN_FLASH_ADDR) / FLASH_PG_SIZE;
 
 	uint32_t addr_pg = MIN_FLASH_ADDR + num_page* FLASH_PG_SIZE;
 
-	flesh_read(addr_pg - MIN_FLASH_ADDR  , (uint16_t*)tmp_buff, FLASH_PG_SIZE);
+	flash_read(addr_pg, (uint16_t*)tmp_buff, FLASH_PG_SIZE);
 
-	flesh_page_erase(num_page);
+	flash_page_erase(num_page);
 
-	memcpy((uint8_t*)(tmp_buff+ (MIN_FLASH_ADDR + addr - addr_pg)), data, size);
+	FLASH->CR |= FLASH_CR_PG;
 
-	uint16_t* addr_tmp = (uint16_t*)addr_pg;
+	memcpy((uint16_t*)(tmp_buff + (addr - addr_pg)), data, size);
 
-	for(uint16_t* indx = (uint16_t*)tmp_buff; indx < (uint16_t*)(tmp_buff + FLASH_PG_SIZE); indx++,addr_tmp++)
-	{
-		*addr_tmp = (*indx);
+	flash_memcpy((uint16_t*)addr_pg, (uint16_t*)tmp_buff, FLASH_PG_SIZE/2);
 
-		if(check_lock()){
-			return TIME_ERROR;
-		}
-	}
-
+	CLEAR_BIT(FLASH->CR, FLASH_CR_PG);
 	return SUCCES;
 }
 
 
-flrslt_t flesh_read(uint32_t addr, uint16_t* data, size_t size)
+flrslt_t flash_read(uint32_t addr, uint16_t* data, size_t size)
 {
-	if(MAX_FLASH_ADDR < (FLASH_OFFSET + addr) || MIN_FLASH_ADDR > (FLASH_OFFSET + addr) || addr & 1){
+	if(MAX_FLASH_ADDR < addr || MIN_FLASH_ADDR > addr || MAX_FLASH_ADDR < addr + size || size & 1){
 		return BAD_INPUT;
 	}
 
-	if(!flash_available()){
-		return FLESH_BLOCK;
-	}
+	flash_available();
 
-	uint32_t end = addr + size;
-
-	for(; addr < end; addr += 2, data++)
-	{
-		*(uint16_t*)data = *((uint16_t*)(FLASH_OFFSET + addr));
-
-		if(check_lock()){
-			return TIME_ERROR;
-		}
-	}
+	flash_memcpy(data, (uint16_t*)addr, size/2);
 
 	return SUCCES;
 }
@@ -106,31 +99,67 @@ bool check_lock()
 }
 
 
-flrslt_t flesh_page_erase(uint8_t npage)
+flrslt_t flash_page_erase(uint8_t npage)
 {
 	if(MAX_NUM_PAGE < npage){
 		return  BAD_INPUT;
 	}
 
-	if(!flash_available()){
-		return FLESH_BLOCK;
-	}
+	flash_available();
 
 	if(check_lock()){
 		return FLESH_BLOCK;
 	}
 
-	FLASH->CR |= FLASH_CR_PER;
-
+	SET_BIT(FLASH->CR, FLASH_CR_PER);
 	FLASH->AR = MIN_FLASH_ADDR + npage * FLASH_PG_SIZE;
+	SET_BIT(FLASH->CR, FLASH_CR_STRT);
 
-	FLASH->CR |= FLASH_CR_STRT;
+	while(FLASH->CR & FLASH_CR_STRT);
 
-	if(!flash_available()){
+	//TODO проверить, что страница стала 0xFFFF
+
+	flash_available();
+
+	CLEAR_BIT(FLASH->CR, FLASH_CR_PER);
+
+	return SUCCES;
+}
+
+flrslt_t flash_mass_erase()
+{
+	flash_available();
+
+	if(check_lock()){
 		return FLESH_BLOCK;
 	}
 
-	//TODO проверить, что страница стала 0xFFFF
+	SET_BIT(FLASH->CR, FLASH_CR_MER);
+	SET_BIT(FLASH->CR, FLASH_CR_STRT);
+
+	while(FLASH->CR & FLASH_CR_STRT);
+
+	//TODO проверить, что все страницы стали 0xFFFF
+
+	flash_available();
+
+	CLEAR_BIT(FLASH->CR, FLASH_CR_MER);
+
+	return SUCCES;
+}
+
+flrslt_t flash_memcpy(uint16_t* dest, uint16_t* src, size_t size)
+{
+	for(; size > 0; size--, dest++, src++)
+	{
+		*dest = *src;
+
+		if(!flash_available()){
+			CLEAR_BIT(FLASH->CR, FLASH_CR_PG);
+			return TIME_ERROR;
+		}
+
+	}
 
 	return SUCCES;
 }
--- fl_lib.h
+++ fl_lib.h
@@ -12,45 +12,49 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#define FLASH_OFFSET		0x08000000U
-#define MAX_NUM_PAGE		63
-#define FLASH_PG_SIZE		1024
-#define MIN_FLASH_ADDR		0x08000000
-#define MAX_FLASH_ADDR		(MIN_FLASH_ADDR + (MAX_NUM_PAGE + 1) * FLASH_PG_SIZE - 1)
+#define MAX_NUM_PAGE		63U
+#define FLASH_PG_SIZE		1024U
+#define MIN_FLASH_ADDR		0x08000000U
+#define MAX_FLASH_ADDR		(MIN_FLASH_ADDR + (MAX_NUM_PAGE + 1U) * FLASH_PG_SIZE - 1U)
 typedef enum
 {
 	SUCCES,				///< Процедура успешная
 	TIME_ERROR,			///< Ошибка таймаута
-	FLESH_BLOCK,		///< Доступ к flesh заблокирован
+	FLESH_BLOCK,		///< Доступ к flash заблокирован
 	BAD_INPUT,
+	WRITE_ERROR,
 	UNKNOWN				///< Неизвестная ошибка
 }flrslt_t;
 
 /**
- * @brief Разблокировка работы с flesh памятью
- * 
- * @return flrslt_t - результат выполениея 
+ * @brief Разблокировка работы с flash памятью
+ *
+ * @return flrslt_t - результат выполениея
  */
-flrslt_t flesh_unlocking();
+flrslt_t flash_unlocking();
 
-void printf_stm32(uint8_t val);
+void flash_locking();
+
+
 /**
- * @brief Сброс flesh памяти
- * 
+ * @brief Сброс flash памяти
+ *
  * @return flrslt_t результат выполения
  */
-flrslt_t flesh_page_erase(uint8_t npage);
+flrslt_t flash_page_erase(uint8_t npage);
 
-flrslt_t flesh_write(uint32_t addr, uint16_t* data, size_t size);
+flrslt_t flash_mass_erase();
 
-flrslt_t flesh_read(uint32_t addr, uint16_t* data, size_t size);
+flrslt_t flash_write(uint32_t addr, uint16_t* data, size_t size);
+
+flrslt_t flash_read(uint32_t addr, uint16_t* data, size_t size);
 
 
 /**
- * @brief Доступность работы с flesh памятью
- * 
+ * @brief Доступность работы с flash памятью
+ *
  * @return true - в случае доступности
- * @return false - в случае если flesh недоступен
+ * @return false - в случае если flash недоступен
  */
 bool flash_available();
 
